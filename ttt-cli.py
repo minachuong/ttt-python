c39c9a6e74c7a1b688f8437c094df67913c7fa74
Mina Chuong <mina.chuong@gmail.com>
2017-05-12 14:43:43 -0700

    added method for taking user input to mark position on board; added loop for swapping players

--- ttt-cli.py
+++ ttt-cli.py
@@ -21,22 +21,56 @@ right_intersect = u'\u2563'
 top_intersect = u'\u2566'
 bottom_intersect = u'\u2569'
 middle_intersect =  u'\u256C'
-p1 = "1"
-p2 = "2"
-p3 = "3"
-p4 = "4"
-p5 = "5"
-p6 = "6"
-p7 = "7"
-p8 = "8"
-p9 = "9"
+
+p = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 # Board
-print(top_left_corner + horizontal_line + top_intersect + horizontal_line + top_intersect + horizontal_line + top_right_corner)
-print(vertical_line + p1 + vertical_line + p2 + vertical_line + p3 + vertical_line)
-print(left_intersect + horizontal_line + middle_intersect + horizontal_line + middle_intersect + horizontal_line + right_intersect)
-print(vertical_line + p4 + vertical_line + p5 + vertical_line + p6 + vertical_line)
-print(left_intersect + horizontal_line + middle_intersect + horizontal_line + middle_intersect + horizontal_line + right_intersect)
-print(vertical_line + p7 + vertical_line + p8 + vertical_line + p9 + vertical_line)
-print(bottom_left_corner + horizontal_line + bottom_intersect + horizontal_line + bottom_intersect + horizontal_line + bottom_right_corner)
+def print_board(): 
+		print(top_left_corner + horizontal_line + top_intersect + horizontal_line + top_intersect + horizontal_line + top_right_corner)
+		print(vertical_line + p[0] + vertical_line + p[1] + vertical_line + p[2] + vertical_line)
+		print(left_intersect + horizontal_line + middle_intersect + horizontal_line + middle_intersect + horizontal_line + right_intersect)
+		print(vertical_line + p[3] + vertical_line + p[4] + vertical_line + p[5] + vertical_line)
+		print(left_intersect + horizontal_line + middle_intersect + horizontal_line + middle_intersect + horizontal_line + right_intersect)
+		print(vertical_line + p[6] + vertical_line + p[7] + vertical_line + p[8] + vertical_line)
+		print(bottom_left_corner + horizontal_line + bottom_intersect + horizontal_line + bottom_intersect + horizontal_line + bottom_right_corner)
+
+print_board()
+
+turn_counter = 1
+
+def user_input(turn_counter):
+		user = int(raw_input("Player: Mark your position: "))
+		if (turn_counter % 2 != 0):
+				p[user - 1] = "X"
+		else:
+				p[user - 1] = "O"
+
+		print(turn_counter)
+
+		print_board()
+		# return turn_counter
+
+
+x = 1
+while(x < 9): 
+	user_input(turn_counter)  
+	turn_counter = turn_counter + 1
+	print(turn_counter)
+	x += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
